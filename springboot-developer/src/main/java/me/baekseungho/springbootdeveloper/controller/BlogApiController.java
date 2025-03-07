b62570471a10cfdb3bc48d6a729a37d477d778d7
baekseungho <mjbsh02@mju.ac.kr>
2025-03-07 17:41:17 +0900

    Feat: 글 조회 요청 매핑 메서드 추가

--- springboot-developer/src/main/java/me/baekseungho/springbootdeveloper/controller/BlogApiController.java
+++ springboot-developer/src/main/java/me/baekseungho/springbootdeveloper/controller/BlogApiController.java
@@ -9,10 +9,7 @@ import me.baekseungho.springbootdeveloper.service.BlogService;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -42,5 +39,14 @@ public class BlogApiController {
 
         return ResponseEntity.ok()
                 .body(articles);
+    }
+
+    @GetMapping("/api/articles/{id}")
+    // @PathVariable URL 경로에서 값 추출
+    public ResponseEntity<ArticleResponse> findArticle(@PathVariable long id) {
+        Article article = blogService.findById(id);
+
+        return ResponseEntity.ok()
+                .body(new ArticleResponse(article));
     }
 }
