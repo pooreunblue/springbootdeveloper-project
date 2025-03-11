b4d163da73df81fb6fcd465b055b25b9e917e71a
baekseungho <mjbsh02@mju.ac.kr>
2025-03-11 16:26:33 +0900

    Feat: UserRepository 생성

---
+++ springboot-developer/src/main/java/me/baekseungho/springbootdeveloper/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package me.baekseungho.springbootdeveloper.repository;
+
+import me.baekseungho.springbootdeveloper.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
